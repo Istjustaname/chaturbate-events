09d48f08b5f8cc2a2bbb612a5a8c6c38fe5c73c1
Pascal <freak9191@web.de>
2019-02-15 19:30:54 +0100

    asdfdasf

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ const logging = new Console(process.stdout, process.stderr);
 
 /**
  * Loads all known transforms.
- * 
+ *
  * @return {Array<*>}
  * @constant
  */
 const getTransforms = () => {
-  const normalizedPath = path.join(__dirname, 'transforms');
-  const transforms = fs.readdirSync(normalizedPath).map((file) => {
-    debug(`loading transform '${file}'...`);
-    // eslint-disable-next-line global-require
-    return require(`./transforms/${file}`);
-  });
+	const normalizedPath = path.join(__dirname, 'transforms');
+	const transforms = fs.readdirSync(normalizedPath).map((file) => {
+		debug(`loading transform '${file}'...`);
+		// eslint-disable-next-line global-require
+		return require(`./transforms/${file}`);
+	});
 
-  return transforms.filter((t) => Boolean(t.event));
+	return transforms.filter((t) => Boolean(t.event));
 };
 
 /**
@@ -29,128 +29,127 @@ const getTransforms = () => {
  */
 class ChaturbateEvents extends EventEmitter {
 
-  /**
-   * Constructor.
-   * 
-   * @param {ChaturbateBrowser} browser 
-   * @param {Array<*>=} transforms
-   * @constructor
-   * @extends EventEmitter
-   */
-  constructor(browser, transforms = null) {
-    super();
-    this.browser = browser;
-    this.browser.on('init', (params) => this._onInit(params));
-    this.browser.on('hooked', (params) => this._onHooked(params));
-    this.browser.on('open', () => this._onOpen());
-    this.browser.on('message', (params) => this._onMessage(params));
-    this.browser.on('error', (error) => this._onError(error));
-    this.browser.on('close', (params) => this._onClose(params));
+	/**
+	 * Constructor.
+	 *
+	 * @param {ChaturbateBrowser} browser
+	 * @param {Array<*>=} transforms
+	 * @constructor
+	 * @extends EventEmitter
+	 */
+	constructor(browser, transforms = null) {
+		super();
+		this.browser = browser;
+		this.browser.on('init', (params) => this._onInit(params));
+		this.browser.on('hooked', (params) => this._onHooked(params));
+		this.browser.on('open', () => this._onOpen());
+		this.browser.on('message', (params) => this._onMessage(params));
+		this.browser.on('error', (error) => this._onError(error));
+		this.browser.on('close', (params) => this._onClose(params));
 
-    this.transforms = transforms || getTransforms();
-  }
+		this.transforms = transforms || getTransforms();
+	}
 
-  /**
-   * Gets a list of known event names.
-   * 
-   * @type {Array<string>}
-   */
-  get names() {
-    return this.transforms.map((t) => t.event);
-  }
+	/**
+	 * Gets a list of known event names.
+	 *
+	 * @type {Array<string>}
+	 */
+	get names() {
+		return this.transforms.map((t) => t.event);
+	}
 
-  /**
-   * Called when the browser is initialized.
-   *
-   * @param {Object} e
-   * @private
-   */
-  _onInit(e) {
-    debug('initialized');
+	/**
+	 * Called when the browser is initialized.
+	 *
+	 * @param {Object} e
+	 * @private
+	 */
+	_onInit(e) {
+		debug('initialized');
 
-    this.emit('init', e);
-  }
+		this.emit('init', e);
+	}
 
-  /**
-   * Called when the websocket has been hooked.
-   *
-   * @param {Object} e
-   * @private
-   */
-  _onHooked(e) {
-    debug('websocket hooked');
+	/**
+	 * Called when the websocket has been hooked.
+	 *
+	 * @param {Object} e
+	 * @private
+	 */
+	_onHooked(e) {
+		debug('websocket hooked');
 
-    this.emit('socket_hooked', e);
-  }
+		this.emit('socket_hooked', e);
+	}
 
-  /**
-   * Called when the websocket is open.
-   *
-   * @private
-   */
-  _onOpen() {
-    debug('websocket open');
+	/**
+	 * Called when the websocket is open.
+	 *
+	 * @private
+	 */
+	_onOpen() {
+		debug('websocket open');
 
-    this.emit('socket_open');
-  }
+		this.emit('socket_open');
+	}
 
-  /**
-   * Called when the websocket forwards a message.
-   *
-   * @param {Object} e
-   * @private
-   */
-  _onMessage(e) {
-    debug(`websocket message: ${e.method}`);
+	/**
+	 * Called when the websocket forwards a message.
+	 *
+	 * @param {Object} e
+	 * @private
+	 */
+	_onMessage(e) {
+		debug(`websocket message: ${e.method}`);
 
-    const transformed = this.transforms.some((t) => {
-      if (t.method !== e.method) return false;
-      if (t.match && !t.match.apply(this, e.args)) return false;
-      if (t.callback && !t.callback.call(this, e.callback)) return false;
+		const transformed = this.transforms.some((t) => {
+			if (t.method !== e.method) return false;
+			if (t.match && !t.match.apply(this, e.args)) return false;
+			if (t.callback && !t.callback.call(this, e.callback)) return false;
 
-      try {
-        const result = t.transform.apply(this, e.args);
-        debug(`transformed to '${t.event}' event`);
-        debug(result);
-        this.emit(t.event, result);
-      } catch (err) {
-        logging.error(`transform failed for '${e.event}'`);
-        logging.error(err);
-      }
+			try {
+				const result = t.transform.apply(this, e.args);
+				debug(`transformed to '${t.event}' event`);
+				debug(result);
+				this.emit(t.event, result);
+			} catch (err) {
+				logging.error(`transform failed for '${e.event}'`);
+				logging.error(err);
+			}
 
-      return true;
-    });
+			return true;
+		});
 
-    if (!transformed) {
-      logging.warn('unable to find matching transform');
-      logging.warn(e);
-    }
-  }
+		if (!transformed) {
+			logging.warn('unable to find matching transform');
+			logging.warn(e);
+		}
+	}
 
-  /**
-   * Called when the websocket throws and error.
-   *
-   * @param {Error} error
-   * @private
-   */
-  _onError(error) {
-    debug('websocket error', error);
-    logging.error(error);
-    this.emit('socket_error', error);
-  }
+	/**
+	 * Called when the websocket throws and error.
+	 *
+	 * @param {Error} error
+	 * @private
+	 */
+	_onError(error) {
+		debug('websocket error', error);
+		logging.error(error);
+		this.emit('socket_error', error);
+	}
 
-  /**
-   * Called when the websocket closes.
-   *
-   * @param {Event} e 
-   * @private
-   */
-  _onClose(e) {
-    debug('websocket close', e);
+	/**
+	 * Called when the websocket closes.
+	 *
+	 * @param {Event} e
+	 * @private
+	 */
+	_onClose(e) {
+		debug('websocket close', e);
 
-    this.emit('socket_close', e);
-  }
-
+		this.emit('socket_close', e);
+	}
 }
 
-export ChaturbateEvents;
+module.exports = ChaturbateEvents;
